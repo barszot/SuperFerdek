cec898b063fce12d2f76773560b616da3c72ba69
bjazwiec <01178226@pw.edu.pl>
2023-05-28 23:58:01 +0200

    pazdzioch dziala

--- src/pazdzioch.cpp
+++ src/pazdzioch.cpp
@@ -1,5 +1,4 @@
 #include "pazdzioch.h"
-#include <iostream>
 Pazdzioch::Pazdzioch()
 {
 
@@ -19,14 +18,24 @@ Pazdzioch::~Pazdzioch()
 void Pazdzioch::setup()
 {
     this->type = -1;
-    this->texture.loadFromFile("src/imgs/pazdzioch.png");
+    this->texture.loadFromFile("src/imgs/enemies.png");
     sprite.setTexture(this->texture);
     sprite.setPosition(this->position);
     sprite.setOrigin(0, 32);
     sprite.setScale(sf::Vector2f (2.0f, 2.0f));
     this->speed = 100.f;
+    this->current_frame = 0;
+    this->max_three_frames_time = 1.f;
+    this->current_three_frames_time = max_three_frames_time;
+    this->pazdzioch_texture_list[0] = sf::IntRect(0, 32, 16, 16);
+    this->pazdzioch_texture_list[1] = sf::IntRect(16, 32, 16, 16);
+    this->pazdzioch_texture_list[2] = sf::IntRect(32, 32, 16, 16);
+    this->pazdzioch_texture_list[3] = sf::IntRect(16, 32, 16, 16);
+
+    this->sprite.setTextureRect(pazdzioch_texture_list[current_frame]);
+
 }
-void Pazdzioch::MarkForDeathIfNecessary(sf::Vector2f ferdek_position, bool is_ferdek_big)
+void Pazdzioch::mark_for_death_if_necessary(sf::Vector2f ferdek_position, bool is_ferdek_big)
 {
 
     if(abs(ferdek_position.x - position.x)<=24.f && (abs(ferdek_position.y-position.y)<=36.f && abs(ferdek_position.y-position.y) > 28.f))
@@ -48,5 +57,45 @@ void Pazdzioch::MarkForDeathIfNecessary(sf::Vector2f ferdek_position, bool is_fe
             can_kill_ferdek = false;
             current_time_of_not_killing_ferdek = max_time_of_not_killing_ferdek;
         }
+    }
+}
+void Pazdzioch::update(float delta_time)
+{
+    move(delta_time);
+    if(current_time_of_not_killing_ferdek>0.f)
+    {
+        current_time_of_not_killing_ferdek -= delta_time;
+    }
+    if(current_time_of_not_killing_ferdek<=0.f)
+    {
+        can_kill_ferdek = true;
+    }
+    int new_frame = current_frame;
+    current_three_frames_time -= delta_time;
+    if(current_three_frames_time<=max_three_frames_time/4.f)
+    {
+        new_frame = 3;
+        if(current_three_frames_time<0.f)
+        {
+            current_three_frames_time = max_three_frames_time;
+        }
+    }
+    else if(current_three_frames_time>max_three_frames_time/4.f && current_three_frames_time<=max_three_frames_time/2.f)
+    {
+        new_frame = 2;
+    }
+    else if(current_three_frames_time>max_three_frames_time/2.f && current_three_frames_time<=3*max_three_frames_time/4.f)
+    {
+        new_frame = 1;
+    }
+    else
+    {
+        new_frame = 0;
+    }
+
+    if(current_frame!=new_frame)
+    {
+        current_frame = new_frame;
+        this->sprite.setTextureRect(pazdzioch_texture_list[current_frame]);
     }
 }
--- src/pazdzioch.h
+++ src/pazdzioch.h
@@ -5,11 +5,16 @@ class Pazdzioch : public Enemy
 {
     private:
         void setup();
+        float max_three_frames_time;
+        float current_three_frames_time;
+        int current_frame;
+        sf::IntRect pazdzioch_texture_list[4];
     public:
         Pazdzioch();
         Pazdzioch(sf::Vector2f position);
         ~Pazdzioch();
-        virtual void MarkForDeathIfNecessary(sf::Vector2f ferdek_position, bool is_ferdek_big);
+        virtual void mark_for_death_if_necessary(sf::Vector2f ferdek_position, bool is_ferdek_big);
+        virtual void update(float delta_time);
 
 };
 
